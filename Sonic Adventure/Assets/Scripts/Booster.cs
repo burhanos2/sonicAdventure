219d0d6449e32f1b70afe00681e50dceaf1e5003
jochemlam <30724588+jochemlam@users.noreply.github.com>
2018-12-10 09:58:54 +0100

    booster early design

---
+++ Sonic Adventure/Assets/Scripts/Booster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour {
+
+    public Rigidbody rb;
+    public Transform player;
+    public AudioSource audioFile;
+
+    public float boostPower;
+
+    private void Awake()
+    {
+        player.GetComponent<Transform>();
+        rb.GetComponent<Rigidbody>();
+        audioFile.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider playerCollider)
+    {
+        rb.AddForce(player.forward * boostPower, ForceMode.VelocityChange);
+        audioFile.Play(0);
+    }
+    
+}
