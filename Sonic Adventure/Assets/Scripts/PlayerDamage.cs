0fd3307741371e52ec7cc144e7e6adecb08eab54
BrandonC <brandoncoenraads@gmail.com>
2018-12-11 12:37:37 +0100

    rings
    ring invincibility
    ring timer

---
+++ Sonic Adventure/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamage : MonoBehaviour
+{
+    public Rigidbody playerRB;
+
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    void OnDamage()
+    {
+        LoseRings();
+        AddKnockback(2);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Spike")
+        {
+            OnDamage();
+        }
+        
+    }
+
+    private void LoseRings()
+    {
+
+    }
+
+    private void AddKnockback(float range)
+    {
+        playerRB.AddForce(-transform.forward * range, ForceMode.Impulse);
+        playerRB.AddForce(transform.up * range, ForceMode.Impulse);
+    }
+}
