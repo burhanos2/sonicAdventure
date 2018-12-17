e4e9b5fcae8be2d14815e77e10509804a924967b
jochemlam <30724588+jochemlam@users.noreply.github.com>
2018-12-17 09:46:08 +0100

    finished my part of playerdamage

--- Sonic Adventure/Assets/Scripts/PlayerDamage.cs
+++ Sonic Adventure/Assets/Scripts/PlayerDamage.cs
@@ -5,27 +5,40 @@ public class PlayerDamage : MonoBehaviour
 {
     public Rigidbody playerRB;
     private Pickup pickup;
+    private SceneHandler sceneHandler;
 
     private void Awake()
     {
         pickup = GetComponent<Pickup>();
         playerRB = GetComponent<Rigidbody>();
+        sceneHandler = GetComponent<SceneHandler>();
     }
 
-    void OnDamage()
+    void OnDamage(float horizontalKB, float verticalKB)
     {
         //LoseRings(pickup.count);
         Ringloss();
-        AddKnockback(playerRB, -2, 2);
+        AddKnockback(playerRB, horizontalKB, verticalKB);
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Spike")
         {
-            OnDamage();
+            OnDamage(-2, 2);
+        }
+
+        if (other.gameObject.tag == "Water")
+        {
+            OnDamage(0, 0);
+            RespawnSonic();
         }
         
+    }
+
+    private void RespawnSonic()
+    {
+        sceneHandler.SwitchSceneSingle("Main");
     }
 
     public void AddKnockback(Rigidbody name, float range, float upValue)
@@ -34,7 +47,9 @@ public class PlayerDamage : MonoBehaviour
         name.AddForce(transform.up * upValue, ForceMode.Impulse);
     }
 
-/*    private void LoseRings(int amount)
+/*    Dit stuk is voor het fancy ring drop effect wat te veel moeite was voor iets dat niet in de 10sec clip zat.
+     
+     private void LoseRings(int amount)
     {
         float oldAngle = 0;
         float newAngle = oldAngle;
@@ -57,15 +72,12 @@ public class PlayerDamage : MonoBehaviour
 
     private void Ringloss()
     {
-        if (pickup.count <= 20) {
-            pickup.count -= pickup.count ;
-            pickup.SetRings();
+        pickup.count -= pickup.count;
+        pickup.SetRings();
 
-        } else {
-
-            pickup.count -= pickup.count;
-            pickup.SetRings();
+        if (pickup.count < 0)
+        {
+            pickup.count = 0;
         }
-
     }
 }
