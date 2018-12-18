d19bfee7c7fc9a1831513643267ef39cc8bb6b68
jochemlam <30724588+jochemlam@users.noreply.github.com>
2018-12-18 12:39:23 +0100

    mostly derped around with the shitty animators

---
+++ Sonic Adventure/Assets/Scripts/PlayerAnims.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnims : MonoBehaviour {
+
+    public Animator sonicAnimator;
+    private PlayerMovement movementScript;
+    private Animation anim;
+    private float walkSpeed = 1.1f;
+    private float runSpeed = 2.9f;
+
+    private void Awake()
+    {
+        
+        movementScript = GetComponent<PlayerMovement>();
+        sonicAnimator = GetComponent<Animator>();
+    }
+
+    private void AdjustAnimation() {
+
+        if (movementScript.rb.velocity.magnitude <= 0)
+        {
+            anim.Play("Idle", PlayMode.StopAll);
+        }
+        if (movementScript.rb.velocity.magnitude > 0 && movementScript.rb.velocity.magnitude < walkSpeed)
+        {
+            anim.Play("Walk", PlayMode.StopAll);
+        }
+        if (movementScript.rb.velocity.magnitude > runSpeed)
+        {
+            anim.Play("Run", PlayMode.StopAll);
+        }
+    }
+
+    public void DamageAnimation()
+    {
+        anim.Play("Hit", PlayMode.StopAll);
+    }
+}
