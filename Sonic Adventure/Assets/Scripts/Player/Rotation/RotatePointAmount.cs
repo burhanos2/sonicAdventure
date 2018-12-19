5a7a9d98ce11c9669c80e29e60f239a08d5eb0e4
Burhanos2 <24742@ma-web.nl>
2018-12-19 23:21:58 +0100

    player turn around

---
+++ Sonic Adventure/Assets/Scripts/Player/Rotation/RotatePointAmount.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePointAmount : MonoBehaviour {
+
+    public short timesToRotate45Deg;
+}
---
+++ Sonic Adventure/Assets/Scripts/Player/Rotation/TurningOnPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningOnPoints : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "RotatePoint")
+        {
+            Rotate45Deg(other.gameObject.GetComponent<RotatePointAmount>().timesToRotate45Deg);
+        }
+    }
+
+
+    private void Rotate45Deg(int times)
+    { transform.Rotate(Vector3.up * Time.deltaTime * 2250 * times, Space.Self); }
+
+}
