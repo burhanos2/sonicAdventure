d9d1d57c5eff3b08fc755d0e301ad3e47d6a6d4d
MSI\user <semmyking@gmail.com>
2018-12-20 02:10:21 +0100

    kutzooi dit meen je niet
    heb slaap nodig

---
+++ Sonic Adventure/Assets/RestartLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartLevel : MonoBehaviour {
+
+    private SceneHandler restart;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            restart.SwitchSceneSingle("Main");
+        }	
+	}
+    private void Awake()
+    {
+        restart = GetComponent<SceneHandler>();
+    }
+}
